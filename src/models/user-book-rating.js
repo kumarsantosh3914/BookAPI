67f9aa8e797f693a5e488d284daeb0d480b7c968
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-10-11 18:11:08 +0530

    user-book-rating, genre, and book-shelf added

---
+++ src/models/user-book-rating.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userBookRatingSchema = new mongoose.Schema(
+  {
+    bookId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+      required: [true, "can't be blank"],
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "can't be blank"],
+    },
+    rating: {
+      type: Number,
+      required: [true, "can't be blank"],
+    },
+  },
+  { timestamps: true }
+);
+
+const UserBookRatingModel = mongoose.model(
+  "UserBookRating",
+  userBookRatingSchema
+);
+module.exports = UserBookRatingModel;
