2c7242ccafc6a7484cd21e302e677103a9be5395
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-10-11 07:19:47 +0530

    user-controller, validation-error code added

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,60 @@
+const { StatusCodes } = require("http-status-codes");
+const { UserService } = require("../services/index");
+
+const {
+  internalServerErrorResponse,
+  customErrorResponse,
+} = require("../utils/common/response-objects");
+
+class UserController {
+  constructor() {
+    this.userService = new UserService();
+  }
+
+  signup = async (req, res) => {
+    try {
+      const user = await this.userService.signup({
+        email: req.body.email,
+        password: req.body.password,
+        username: req.body.username,
+      });
+      return res.status(StatusCodes.CREATED).json({
+        message: "Successfully created the User",
+        err: {},
+        data: user,
+        success: true,
+      });
+    } catch (error) {
+      if (!error.statusCode) {
+        return res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json(internalServerErrorResponse(error));
+      }
+      return res.status(error.statusCode).json(customErrorResponse(error));
+    }
+  };
+
+  signin = async (req, res) => {
+    try {
+      const response = await this.userService.signin({
+        email: req.body.email,
+        password: req.body.password,
+      });
+      return res.status(StatusCodes.OK).json({
+        message: "Successfully signed in",
+        data: response,
+        err: {},
+        success: true,
+      });
+    } catch (error) {
+      if (!error.statusCode) {
+        return res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json(internalServerErrorResponse(error));
+      }
+      return res.status(error.statusCode).json(customErrorResponse(error));
+    }
+  };
+}
+
+module.exports = new UserController();
---
+++ src/utils/errors/validation-error.js
@@ -0,0 +1,16 @@
+const { StatusCodes } = require("http-status-codes");
+class ValidationError extends Error {
+  constructor(error) {
+    super();
+    let explanation = [];
+    Object.keys(error.errors).forEach((field) => {
+      explanation.push(error.errors[field]);
+    });
+    this.name = "ValidationError";
+    this.message = error.message;
+    this.explanation = explanation;
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+module.exports = ValidationError;
