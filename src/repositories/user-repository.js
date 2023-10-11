12d7426c80a804301cd2ce57bfc7c2dba61073a3
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-10-11 06:33:02 +0530

    user-repository and clientError code added

---
+++ src/repositories/user-repository.js
@@ -0,0 +1,41 @@
+const { StatusCodes } = require("http-status-codes");
+const User = require("../models/user");
+const { ClientError } = require("../utils/errors");
+const CrudRepository = require("./crud-repository");
+
+class UserRepository extends CrudRepository {
+  constructor() {
+    super(User);
+  }
+
+  getUserByEmail = async (userEmail) => {
+    try {
+      const user = await User.findOne({
+        email: userEmail,
+      });
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  getUserById = async (id) => {
+    try {
+      const user = await User.findById(id);
+      if (!user) {
+        throw new ClientError(
+          {
+            message: "Invalid date sent from the client",
+            explanation: "No registered user found for the given email",
+          },
+          StatusCodes.NOT_FOUND
+        );
+      }
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  };
+}
+
+module.exports = UserRepository;
---
+++ src/utils/errors/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  ClientError: require("./client-error"),
+};
