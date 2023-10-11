2c121c6d1295484d586c128d4e62e48d68a92b5f
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-10-11 17:51:33 +0530

    book and author model added

---
+++ src/models/book.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
+    description: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Author",
+    },
+    genres: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Genre",
+      },
+    ],
+    pages: {
+      type: Number,
+      required: true,
+    },
+    publishDate: {
+      type: String,
+    },
+    rating: {
+      type: String,
+      default: 0,
+      required: [true, "can't be blank"],
+    },
+  },
+  { timestamps: true }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+module.exports = Book;
