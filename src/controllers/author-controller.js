4851450d1e20764b134348380810680234d88a11
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-10-11 18:58:35 +0530

    author controller added

---
+++ src/controllers/author-controller.js
@@ -0,0 +1,55 @@
+const { StatusCodes } = require("http-status-codes");
+
+const AuthorService = require("../services/author-service");
+const {
+  internalServerErrorResponse,
+  customErrorResponse,
+} = require("../utils/common/response-objects");
+
+class AuthorController {
+  constructor() {
+    this.authorController = new AuthorController();
+  }
+
+  create = async (req, res) => {
+    try {
+      const author = await this.authorController.create({
+        name: req.body.name,
+      });
+      return res.status(StatusCodes.CREATED).json({
+        message: "Successfully created the Author",
+        err: {},
+        data: author,
+        success: true,
+      });
+    } catch (error) {
+      if (!error.statusCode) {
+        return res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json(internalServerErrorResponse(error));
+      }
+      return res.status(error.statusCode).json(customErrorResponse(error));
+    }
+  };
+
+  getAll = async (req, res) => {
+    try {
+      const authors = await this.AuthorService.getAll();
+      return res.status(StatusCodes.OK).json({
+        message: "Successfully fetched all the Authors",
+        err: {},
+        data: authors,
+        success: true,
+      });
+    } catch (error) {
+      if (!error.statusCode) {
+        return res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json(internalServerErrorResponse(error));
+      }
+      return res.status(error.statusCode).json(customErrorResponse(error));
+    }
+  };
+}
+
+module.exports = AuthorController;
