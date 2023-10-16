c5abb4252af185731cb88f62255ebe59c3b87cbe
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-10-16 19:44:42 +0530

    genre repository added

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -2,4 +2,5 @@ module.exports = {
   UserRepository: require("./user-repository"),
   AuthorRepository: require("./author-repository"),
   BookRepository: require("./book-repository"),
+  GenreRepository: require("./genre-repository"),
 };
