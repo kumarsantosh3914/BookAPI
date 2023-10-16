be7576f53e13c4df79be678daa08d05ce34755f1
Santosh Kumar <santoshkumar15novmth@gmail.com>
2023-10-16 19:54:48 +0530

    genre service added

--- src/services/index.js
+++ src/services/index.js
@@ -2,4 +2,5 @@ module.exports = {
   UserService: require("./user-service"),
   AuthorService: require("./author-service"),
   BookService: require("./book-service"),
+  GenreService: require("./genre-service"),
 };
